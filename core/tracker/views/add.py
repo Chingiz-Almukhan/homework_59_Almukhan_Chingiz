471d3073ae2bb8bf37e659e3d5a51e6542e0096e
Almukhan Chingiz <chingizpain@gmail.com>
2022-10-14 15:23:51 +0600

    Был переопределен метод для добавления задач Был создан метод для добавления проекта Создан шаблоны для добавления задач в существующий проект и шаблон для создания проекта

--- core/tracker/views/add.py
+++ core/tracker/views/add.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
-
 from tracker.forms import AddEditForm
-from tracker.models import IssueTracker
+from tracker.models import IssueTracker, Project
 
 
 class AddView(CreateView):
@@ -11,3 +10,9 @@ class AddView(CreateView):
     success_url = reverse_lazy('main')
     model = IssueTracker
     form_class = AddEditForm
+
+    def form_valid(self, form):
+        project = get_object_or_404(Project, pk=self.kwargs.get('pk'))
+        form.instance.project = project
+        return super(AddView, self).form_valid(form)
+
